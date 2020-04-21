c25630c763462b00666291cb8e57a0f06d0fa67a
millenito <wownito31@gmail.com>
2020-04-21 20:41:00 +0700

    add main menu screen and buttons

--- menu.py
+++ menu.py
@@ -3,16 +3,17 @@ import sys
 
 
 class Button():
-    def __init__(self, color, x, y, width, height, text=''):
+    def __init__(self, color, x, y, width, height, text='', text_color=(0, 0, 0)):
         self.color = color
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
+        self.text_color = text_color
 
-    def draw(self, win, outline=None, font_color=(0, 0, 0)):
-        # Call this method to draw the button on the screen
+    # Call this method to draw the button on the screen
+    def draw(self, win, outline=None):
         if outline:
             pygame.draw.rect(win, outline, (self.x-2, self.y -
                                             2, self.width+4, self.height+4), 0)
@@ -22,7 +23,7 @@ class Button():
 
         if self.text != '':
             font = pygame.font.Font("freesansbold.ttf", 32)
-            text = font.render(self.text, 1, font_color)
+            text = font.render(self.text, 1, self.text_color)
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2),
                             self.y + (self.height/2 - text.get_height()/2)))
 
@@ -57,25 +58,50 @@ class Menu:
                           (self.screen_h / 2) - 50,
                           250,
                           100,
-                          "Single Player")
+                          "Single Player",
+                          self.white)
         b_multi = Button(self.bg_color,
                          (self.screen_w / 2) - 120,
-                         (self.screen_h / 2) - 20,
+                         (self.screen_h / 2) + 90,
                          250,
                          100,
-                         "Multi Player")
+                         "Multi Player",
+                         self.white)
         while True:
             for event in pygame.event.get():
+                mouse_pos = pygame.mouse.get_pos()
+
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if b_single.isOver(mouse_pos):
+                        print('click single player')
+                    elif b_multi.isOver(mouse_pos):
+                        print('click multi player')
+
+                if event.type == pygame.MOUSEMOTION:
+                    if b_single.isOver(mouse_pos):
+                        b_single.color = self.white
+                        b_single.text_color = self.bg_color
+                    else:
+                        b_single.color = self.bg_color
+                        b_single.text_color = self.white
+
+                    if b_multi.isOver(mouse_pos):
+                        b_multi.color = self.white
+                        b_multi.text_color = self.bg_color
+                    else:
+                        b_multi.color = self.bg_color
+                        b_multi.text_color = self.white
 
             self.screen.fill(self.bg_color)
             self.render_text('Pong', self.white,
                              self.screen_w / 2,
                              (self.screen_h / 2) - 270,
                              center=True)
-            b_single.draw(self.screen, self.white, self.white)
-            b_multi.draw(self.screen, self.white, self.white)
+            b_single.draw(self.screen, self.white)
+            b_multi.draw(self.screen, self.white)
 
             pygame.display.flip()  # Outputs to display
